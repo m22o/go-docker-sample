ad9aa3c1d3703172329d717207f66f9d12458539
m2o mobile pc <takuro.matsuo@dena.com>
2018-04-07 19:51:47 +0900

    go on nginx

--- server.go
+++ server.go
@@ -1,12 +1,21 @@
 package main
 import (
     "fmt"
+    "net"
     "net/http"
+    "net/http/fcgi"
+
 )
 
 func main() {
+    // http.HandleFunc("/", handler)
+    // http.ListenAndServe(":8080", nil)
+    l, err := net.Listen("tcp", ":8080")
+    if err != nil {
+        return
+    }
     http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+    fcgi.Serve(l, nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
